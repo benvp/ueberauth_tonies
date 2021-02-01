4d093f1eaefa1a1bf29eb7289dd9980571e5cbe1
Benjamin von Polheim <ben@vp.solutions>
2021-02-01 09:43:25 +0100

    add metadata

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule UeberauthTonies.MixProject do
   use Mix.Project
 
+  @url "https://github.com/benvp/ueberauth_tonies"
+
   def project do
     [
       app: :ueberauth_tonies,
       version: "0.1.0",
+      description: "Ueberauth strategy for Tonies.",
+      source_url: @url,
+      homepage_url: @url,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -22,7 +29,21 @@ defmodule UeberauthTonies.MixProject do
   defp deps do
     [
       {:oauth2, "~> 2.0"},
-      {:ueberauth, "~> 0.6"}
+      {:ueberauth, "~> 0.6"},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [extras: ["README.md"]]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Benjamin von Polheim"],
+      licenses: ["MIT"],
+      links: %{GitHub: @url}
     ]
   end
 end
